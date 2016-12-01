6dd2d1e76eadc379b81cda2f9a32e316f771d5b9
Sean McCarthy <spm64@pitt.edu>
2016-12-01 13:50:38 -0500

    add google link

--- assignment4.js
+++ assignment4.js
@@ -65,8 +65,6 @@ function handleSearch(searchString){
     }
     matches = tempMatch;
   }
-
-  console.log(matches);
   updateMatchesDisplay();
 }
 
@@ -76,8 +74,6 @@ function updateMatchesDisplay(){
 
   // Draw new search results
   for(match of matches){
-    $(".results .results-list").append("<li>" + match + "</li>");
+    $(".results .results-list").append("<li><a target='_blank' href='https://www.google.com/#q=" + match + "'>" + match + "</a></li>");
   }
-
-
 }
